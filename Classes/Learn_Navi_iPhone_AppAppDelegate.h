a9caa57978c89e8136d853ab72f2c9a36e7baa8f
Seze Ngrr <seze@learnnavi.org>
2010-01-09 01:26:02 +0000

    Initial Import
    git-svn-id: http://learn-navi.googlecode.com/svn/trunk@2 4dd590c8-5356-56d4-01ec-4945be1ffd85

---
+++ Classes/Learn_Navi_iPhone_AppAppDelegate.h
@@ -0,0 +1,29 @@
+//
+//  Learn_Navi_iPhone_AppAppDelegate.h
+//  Learn Navi iPhone App
+//
+//  Created by Michael Gillogly on 1/8/10.
+//  Copyright __MyCompanyName__ 2010. All rights reserved.
+//
+
+@interface Learn_Navi_iPhone_AppAppDelegate : NSObject <UIApplicationDelegate> {
+    
+    NSManagedObjectModel *managedObjectModel;
+    NSManagedObjectContext *managedObjectContext;	    
+    NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+    UIWindow *window;
+    UINavigationController *navigationController;
+}
+
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+- (NSString *)applicationDocumentsDirectory;
+
+@end
+
